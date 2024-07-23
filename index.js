d9f1110382e5e60d193b6e6dd4d71e9d2a4ca02c
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-07-23 09:49:15 +0300

    practices fetch

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON data
+  })
+  .then(books => {
+    renderBooks(books); // Call renderBooks with the array of books
+  })
+  .catch(error => {
+    console.error('Error fetching books:', error);
+  });
 }
 
 function renderBooks(books) {
